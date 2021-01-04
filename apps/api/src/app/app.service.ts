4d7f1adfb15fd1d821f9c18a8fe30c7b6fab9d20
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2021-01-04 21:29:05 +0100

    fix tests

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to Aurum api!' };
+    return { message: 'Welcome to api!' };
   }
 }
