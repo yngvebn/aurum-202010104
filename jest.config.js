4df74661031ffcc5e2b6e324340dff1c8bf4de5e
Yngve Bakken-Nilsen <yngve.nilsen@gmail.com>
2021-01-04 21:23:38 +0100

    initial commit

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/fagkveld', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/fagkveld',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/ui',
+  ],
 };
